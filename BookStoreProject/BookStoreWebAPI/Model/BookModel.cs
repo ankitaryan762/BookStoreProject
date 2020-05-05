62b05f8aaa37dbbb061842197b9c2a5a7bd73b16
sumitchourasia <st.sumit01@gmail.com>
2020-05-06 05:22:44 +0530

    [sumit] Add . added BookModel class in Model project Module

---
+++ BookStoreProject/BookStoreWebAPI/Model/BookModel.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Model
+{
+    /// <summary>
+    /// Model class for book
+    /// </summary>
+    public class BookModel
+    {
+        /// <summary>
+        /// property BookId as a Primary Key of the Book Table.
+        /// </summary>
+        public long BookId { get; set; }
+
+        /// <summary>
+        /// property for the Title of the book
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// property for the Author of the book
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// property for Price of the book
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// property for Cover Image of the book
+        /// </summary>
+        public string Image { get; set; }
+    }
+}

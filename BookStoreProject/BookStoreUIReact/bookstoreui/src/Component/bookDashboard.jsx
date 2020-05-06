e74667963e288b559d2bad24ee9e9042fe6d7156
Anjalik19 <anjali.march19.com>
2020-05-06 23:41:58 +0530

    [Anjali]Add. Created dashboard for books

--- BookStoreProject/BookStoreUIReact/bookstoreui/src/Component/bookDashboard.jsx
+++ BookStoreProject/BookStoreUIReact/bookstoreui/src/Component/bookDashboard.jsx
@@ -1,34 +1,83 @@
-import React,{ Component } from "react";
-import { Typography, CardActions,Button,Card } from '@material-ui/core';
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Pagination from '@material-ui/lab/Pagination';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import logo from '../logo.svg';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import dragon from '../assets/dragon.jpg'
+
 class BookDashboard extends Component {
-    constructor(props) {
-        super();
-    }
     render() {
-        this.props.books.map((data)=>{
-        return(
+        return (
             <div>
-                    <Card>
-                        <Card id="book-detail">
-                            <Typography variant="h1" component="h2">
-                                {data.bookName}
-                            </Typography>
-                            <Typography variant="h1" component="h2">
-                                {data.bookId}
-                            </Typography>
-                            <Typography variant="h1" component="h2">
-                                {data.authorName}
-                            </Typography>
-                        </Card>
-                        <CardActions>
-                        <Button variant="contained">Add to cart</Button>
-                        <Button variant="contained">Wishlist</Button>
+                <div className='title-div'>
+                    <Typography variant='h6'>
+                        Books
+                        </Typography>
+                    <div>
+                        <select name="Sort By Relevance" className='sort' >
+                            <option selected>Sort By Relevance</option>
+                            <option>Price:low to high</option>
+                            <option>Price:high to low</option>
+                            <option>Newest Arrivals</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='display-books'>
+                {
+                    this.props.books.map((data)=>{
+                        return(
+                            <Card className='note-card' >
+                                <div className="image-div">
+                                <img src={dragon} style={styles.imgStyle} />
+                                </div>
+                        
+                            <CardContent id='card-detail'>
+                                <Typography  variant="h7" component="h4">
+                                    {data.bookName}
+                                            </Typography>
+                                <Typography>
+                                    {data.authorName}
+                                            </Typography>
+                                <Typography>
+                                    {data.price}
+                                            </Typography>
+                            </CardContent>
+                        <CardActions className="buttons">
+                            <Button 
+                            variant='outlined'
+                                color='default'
+                            > Add to cart</Button>
+
+                            <Button
+                                variant='outlined'
+                                color='default'
+                            > Wishlist</Button>
                         </CardActions>
                     </Card>
+
+                        );
+                    })
+                }
+                    
                 </div>
+                {/* <div className='pagination-div'>
+                    <Pagination  count={10} color="primary" />
+                </div> */}
+            </div>
+        )
 
+    }
 
-            )
-        })
+}
+const styles = {
+    imgStyle:{
+        height:170,
+        width:170,
     }
 }
+export default BookDashboard;
--- BookStoreProject/BookStoreUIReact/bookstoreui/src/Component/dashboard.jsx
+++ BookStoreProject/BookStoreUIReact/bookstoreui/src/Component/dashboard.jsx
@@ -1,29 +1,62 @@
-import React,{ Component } from "react";
-import bookDashboard from '../Component/bookDashboard'
-import { withRouter } from "react-router-dom";
-class Dashboard extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            books:[
-                {
-                    bookName:"abc",
-                    bookId:123,
-                    bookAuthor:"Anjali"
-                },
-                {
-                    bookName:"shero",
-                    bookId:123,
-                    bookAuthor:"shero"
+import React,{Component} from 'react';
+import BookDashboard from './bookDashboard';
+import Header from './header'
 
-                },
-            ]
-        }
+class Dashboard extends Component{
+    state={
+        books:[
+        {
+            bookid : 1,
+            bookName : "Shero",
+            authorName : "shero",
+            price : 500,
+        },
+        {
+            bookid : 2,
+            bookName : "Jimmy",
+            authorName : "jimmy",
+            price : 400,
+
+        },{
+            bookid : 3,
+            bookName : "Sumit",
+            authorName : "Sumit",
+            price : 550,
+
+        },{
+            bookid : 4,
+            bookName : "Somya",
+            authorName : "Somya",
+            price : 400,
+
+        },{
+            bookid : 5,
+            bookName : "Ankit",
+            authorName : "Ankit",
+            price : 500,
+
+        },{
+            bookid : 6,
+            bookName : "Rajat",
+            authorName : "Rajat",
+            price : 400,
+
+        },{
+            bookid : 7,
+            bookName : "Anjali",
+            authorName : "Anjali",
+            price : 450,
+
+        }]
     }
     render(){
         return(
-<bookDashboard books={this.props.book}/>
+            <div>
+                <Header/>
+            <BookDashboard books={this.state.books}/>
+            </div>
         )
+
     }
-} 
-export default withRouter(Dashboard)
+}
+export default Dashboard;

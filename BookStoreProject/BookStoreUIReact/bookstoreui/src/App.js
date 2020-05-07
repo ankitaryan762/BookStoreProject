7f0220d5dd3fd062cacb786ddb35face1e76832d
Somya <somyacs14@gmail.com>
2020-05-07 08:21:12 -0700

    [Somya]Add. Adding the reander Method in the BookDetails.jsx of the Component folder

--- BookStoreProject/BookStoreUIReact/bookstoreui/src/App.js
+++ BookStoreProject/BookStoreUIReact/bookstoreui/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './Component/Header'
+import BookDetails from './Component/BookDetails'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <BookDetails/>
     </div>
   );
 }

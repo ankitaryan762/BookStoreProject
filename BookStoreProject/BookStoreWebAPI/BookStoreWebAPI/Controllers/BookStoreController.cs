cbd0d5719a10f412a17a2f90b07db258bc0af739
Ankit Aryan <er.ankitaryan@gmail.com>
2020-05-06 19:15:16 -0700

    [Ankit]added. Implementation of BookStorecontroller in controller of BookStoreWebApi project

--- BookStoreProject/BookStoreWebAPI/BookStoreWebAPI/Controllers/BookStoreController.cs
+++ BookStoreProject/BookStoreWebAPI/BookStoreWebAPI/Controllers/BookStoreController.cs
@@ -2,16 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Manager.ManagerIMPL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Model.ModelCLasses;
 
 namespace BookStoreWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BookStoreController : ControllerBase
-    { 
+    {
+        private readonly BookManager bookManager;
 
-        
+        public BookStoreController(BookManager bookManager)
+        {
+            this.bookManager = bookManager;
+        }
+
+        [Route("addbook")]
+        [HttpPost]
+        public async Task<IActionResult> AddBook(BookModel bookModel)
+        {
+            var result = await this.bookManager.AddBook(bookModel);
+            if (result == 1)
+            {
+                return this.Ok(bookModel);
+            }
+                return this.BadRequest();
+        }
+
+        [Route("getallemployee")]
+        [HttpGet]
+        public IEnumerable<BookModel> GetAllBook()
+        {
+            return bookManager.GetAllBook();
+        }
     }
 }
--- BookStoreProject/BookStoreWebAPI/Manager/IManager/IBookManager.cs
+++ BookStoreProject/BookStoreWebAPI/Manager/IManager/IBookManager.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.IManager
 {
    public interface IBookManager
     {
         IEnumerable<BookModel> GetAllBook();
+        Task<int> AddBook(BookModel entity);
     }
 }

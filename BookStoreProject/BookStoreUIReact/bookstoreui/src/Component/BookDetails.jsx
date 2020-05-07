a6dc4b0313c571e77d0fe1e1dedbdc63948bc358
Somya <somyacs14@gmail.com>
2020-05-07 08:18:05 -0700

    [Somya]Add. Adding the BookDetails.jsx in the Component folder

---
+++ BookStoreProject/BookStoreUIReact/bookstoreui/src/Component/BookDetails.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BookDetails extends Component {
+    render() {
+        return (
+        <div>
+            </div>
+        )
+    }
+}
+export default BookDetails;

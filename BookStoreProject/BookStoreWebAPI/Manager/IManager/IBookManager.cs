3f124d9c35a54851129808e6226f2453ad77ecbf
Ankit Aryan <er.ankitaryan@gmail.com>
2020-05-06 17:59:04 -0700

    [Ankit]Added. implementation of Manager folder for achieve abstraction

---
+++ BookStoreProject/BookStoreWebAPI/Manager/IManager/IBookManager.cs
@@ -0,0 +1,12 @@
+﻿using Model.ModelCLasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.IManager
+{
+   public interface IBookManager
+    {
+        IEnumerable<BookModel> GetAllBook();
+    }
+}
